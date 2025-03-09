e48c724ac16be8936eb8d9b52d78b36d78c263ad
mmk4-81 <mmkkarbalaei@gmail.com>
2025-03-09 18:50:22 +0330

    Add animated Home page with Framer Motion and Valtio state management

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useSnapshot } from 'valtio';
 
-import state from '../store';
+import State from '../store';
 import { getContrastingColor } from '../config/helpers';
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
-  const snap = useSnapshot(state);
+  const snap = useSnapshot(State);
 
   const generateStyle = (type) => {
     if(type === 'filled') {
